9934f9b980fdfc3e7e46bba29bb69606fab3fda1
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-15 13:51:10 +0700

    Create class CollectionUtilAdapter implement ColectionOperations

---
+++ src/com/codegym/CollectionUtilsAdapter.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class CollectionUtilsAdapter implements CollectionOperations{
+    @Override
+    public int findMax(Set<Integer> numbers) {
+        List<Integer> listNumber = new ArrayList<>(numbers);
+        CollectionUtils collUntil = new CollectionUtils();
+        return collUntil.findMax(listNumber);
+    }
+}
