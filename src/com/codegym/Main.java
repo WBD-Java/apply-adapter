ca16643381d141e4f28244f2eeea1c533cd44c56
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-15 13:51:58 +0700

    Run class Main

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -1,8 +1,23 @@
 package com.codegym;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 5 numbers: ");
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(scanner.nextInt());
+        numbers.add(scanner.nextInt());
+        numbers.add(scanner.nextInt());
+        numbers.add(scanner.nextInt());
+        numbers.add(scanner.nextInt());
 
+        CollectionOperations coll = new CollectionUtilsAdapter();
+        Client client = new Client(coll);
+        client.printMaxValue(numbers);
     }
 }
